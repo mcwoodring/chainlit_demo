f99cfa26dea29c8c19f0d2fe641ae274df7ee327
Mike Woodring <mike.woodring@codepath.org>
2024-12-03 11:51:19 -0700

    Milestone 4 (refactored)

--- app.py
+++ app.py
@@ -9,32 +9,33 @@ import openai
 
 load_dotenv()
 
-# _config_open_ai = SimpleNamespace(
-#     endpoint_url = "https://api.openai.com/v1",
-#     model_kwargs = {
-#         "model": "chatgpt-4o-latest",   # https://platform.openai.com/docs/models/gpt-4o
-#         "temperature": 0.3,
-#         "max_tokens": 500,
-#     },
-#     message_history_key = 'message_history',
-# )
-# _client_open_ai = openai.AsyncClient(api_key=env['OPENAI_API_KEY'], base_url=_config_open_ai.endpoint_url)
+_configurations = {
+    'openai': SimpleNamespace(
+        ai_name = 'OpenAI',
+        endpoint_url = 'https://api.openai.com/v1',
+        api_key = env['OPENAI_API_KEY'],
+        model_kwargs = {
+            'model': 'chatgpt-4o-latest',   # https://platform.openai.com/docs/models/gpt-4o
+            'temperature': 0.3,
+            'max_tokens': 500,
+        },
+        message_history_key = 'message_history',
+    ),
+    'runpod': SimpleNamespace(
+        ai_name = 'Mistral',
+        endpoint_url = f"https://api.runpod.ai/v2/{env['RUNPOD_SERVERLESS_ID']}/openai/v1",
+        api_key = env['RUNPOD_API_KEY'],
+        model_kwargs = {
+            'model': 'mistralai/Mistral-7B-Instruct-v0.3',
+            'temperature': 0.3,
+            'max_tokens': 500
+        },
+        message_history_key = 'message_history',
+    )
+}
 
-
-_config_runpod = SimpleNamespace(
-    endpoint_url = f"https://api.runpod.ai/v2/{env['RUNPOD_SERVERLESS_ID']}/openai/v1",
-    model_kwargs = {
-        "model": "mistralai/Mistral-7B-Instruct-v0.3",
-        "temperature": 0.3,
-        "max_tokens": 500
-    },
-    message_history_key = 'message_history',
-)
-_client_runpod = openai.AsyncClient(api_key=env['RUNPOD_API_KEY'], base_url=_config_runpod.endpoint_url)
-
-
-_config = _config_runpod
-_client = _client_runpod
+_config = _configurations['runpod']
+_client = openai.AsyncClient(api_key=_config.api_key, base_url=_config.endpoint_url)
 
 # xref https://docs.chainlit.io/concepts/chat-lifecycle
 #
@@ -51,9 +52,9 @@ def on_chat_start():
 async def on_message(message: cl.Message):
     # Maintain an array of messages in the user session
     message_history = cl.user_session.get(_config.message_history_key, [])
-    message_history.append({"role": "user", "content": message.content})
+    message_history.append({'role': 'user', 'content': message.content})
 
-    response_message = cl.Message(content="")
+    response_message = cl.Message(content='')
     await response_message.send()
     
     # Pass in the full message history for each request
@@ -63,14 +64,21 @@ async def on_message(message: cl.Message):
         **_config.model_kwargs
     )
 
+    prefix = f"[{_config.ai_name}] "
+    prefix_streamed = False
+
     async for part in stream:
-        if token := part.choices[0].delta.content or "":
+        if token := part.choices[0].delta.content or '':
+            if not prefix_streamed:
+                await response_message.stream_token(prefix)
+                prefix_streamed = True
+
             await response_message.stream_token(token)
 
     await response_message.update()
 
     # Record the AI's response in the history
-    message_history.append({"role": "assistant", "content": response_message.content})
+    message_history.append({'role': 'assistant', 'content': response_message.content})
     cl.user_session.set(_config.message_history_key, message_history)
 
 
@@ -85,5 +93,5 @@ async def main():
     run_chainlit(__file__)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
